fa6e17b0f414d94edbbeb53f58e2f6ab3a2acc9a
saikondepati35 <saikondepati35@gmail.com>
2023-05-25 08:50:47 +0530

    Initial commit

---
+++ addapp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+def home(request):
+    return render(request,'home.html')
+def add(request):
+    x=request.GET["t1"]
+    y=request.GET["t2"]
+    i=int(x)
+    j=int(y)
+    z=i+j
+    res=HttpResponse("The sum is:"+str(z))
+    return res
+
+
+
